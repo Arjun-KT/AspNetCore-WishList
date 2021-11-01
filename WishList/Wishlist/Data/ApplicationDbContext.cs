5e700bdd60a5a70f727e089cadfa8cc23a6e99bc
Arjun-KT <arjunkt007@gmail.com>
2021-11-01 16:07:08 +0530

    -Added Item and linked to ApplicationDbContext

---
+++ WishList/Wishlist/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WishList.Wishlist.Models;
+
+namespace WishList.Wishlist.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public DbSet<Item> Item {get;set;} 
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
